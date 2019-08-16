5e3b7f920dfb7cf3cc3c7243e562ced6713d2b9e
Keomony <kkhun@thoughtworks.com>
2019-08-16 16:59:38 +0100

    [#10] [Tech-task] (Keomony) Setup a template for springboot web app.

---
+++ src/main/java/fullstacktodolist/todolist/TodoListApplication.java
@@ -0,0 +1,15 @@
+package fullstacktodolist.todolist;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		run(TodoListApplication.class, args);
+	}
+
+}
