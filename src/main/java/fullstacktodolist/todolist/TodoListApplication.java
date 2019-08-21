d7be2c2c5729cb86cb56c560d7396564d490011b
Keomony <kkhun@thoughtworks.com>
2019-08-21 11:41:46 +0100

    [#10] [Tech-task] (Keomony) add more logging messages to build and undo TodoListApplication back to the original version

--- src/main/java/fullstacktodolist/todolist/TodoListApplication.java
+++ src/main/java/fullstacktodolist/todolist/TodoListApplication.java
@@ -1,15 +1,13 @@
 package fullstacktodolist.todolist;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class TodoListApplication {
 
-	public static void main(String[] args) {
-		run(TodoListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplication.class, args);
+    }
 
 }
